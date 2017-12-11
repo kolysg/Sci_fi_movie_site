6355752a6a1150c4fa7a2c442af4d09aac5ffce4
kolysg <koly.sg@gmail.com>
2017-12-11 15:23:44 -0800

    initialized the class instance vars

--- media.py
+++ media.py
@@ -14,20 +14,23 @@ class Movie():
 	'''This class provides a way to store movie related information'''
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 	# constructor
-	def __init__(self, movie_title, youtubeTrailer):
+	def __init__(self, movie_title, youtube_trailer):
 		# using omdbapi to get movie details
 		omdb_base_url = "http://www.omdbapi.com/" 
 		query_by_title = "?t=" + movie_title
 		movies_url = omdb_base_url + query_by_title
 		get_movie_data = urllib2.urlopen(movies_url)
-		json_obj = json.loads(get_movie_data)
+		json_data = json.loads(get_movie_data)
 
-		print json_obj
+		print json_data
 
-		self.title = title
-		self.storyline = storyline
-		self.poster_image_url = posterImage
-		self.trailer_youtube_url = youtubeTrailer
+		self.title = json_data["Title"]
+		self.genre = json_data["Genre"]
+		self.year = json_data["Released"][-4:] #get four digit year
+		self.storyline = json_data["Plot"]
+		self.poster_image_url = json_data["Poster"]
+		self.imdb_rating = json_data["imdbRating"]
+		self.trailer_youtube_url = youtube_trailer
 
     # instance method
 	def show_trailer(self):
